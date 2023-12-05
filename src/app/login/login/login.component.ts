c660ea9b2eab51dc20cf35cf0b52b849dd9c3d9a
Santiago dos Santos <santiiagodossantos@icloud.com>
2023-12-04 21:56:36 -0300

    Landing page styles

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Typewriter } from 'ngx-simple-typewriter';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [Typewriter],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  words = [
+    'Cuentas Claras',
+    'Divide gastos fácilmente',
+    'Pago colaborativo',
+    'Salda cuentas sin conflictos',
+    'Gestión conjunta de gastos',
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
