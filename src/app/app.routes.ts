4e7a5bebd483fb9e23c1a13732c1c744eb16a83e
Cath Gaitan <catu.gaitan@gmail.com>
2023-12-14 12:23:38 -0300

    cambio nombre title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -41,7 +41,7 @@ export const routes: Routes = [
       },
       {
         path: 'expense',
-        title: 'Lista Gastos',
+        title: 'Mis Gastos',
         component: ListExpensesComponent
       },
       {
