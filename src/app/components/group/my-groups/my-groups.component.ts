838c8a08b365b4748a3e85bfb10a50acccb3c034
Sun <santiago.lizondo@gmail.com>
2023-12-14 22:06:53 -0300

    fix imagen subliminal

--- src/app/components/group/my-groups/my-groups.component.ts
+++ src/app/components/group/my-groups/my-groups.component.ts
@@ -58,7 +58,7 @@ export interface Section {
 })
 export class MyGroupsComponent implements OnInit {
   user_id: number = -1;
-  myGroups: Group[] = [];
+  myGroups: Group[] = [{ id: -1, name: '', category: {}}];
 
   constructor(
     private _snackBar: MatSnackBar,
@@ -83,8 +83,8 @@ export class MyGroupsComponent implements OnInit {
         )
         .subscribe(
           (groups) => {
+            this.myGroups.pop();
             this.myGroups = groups;
-            console.log(this.myGroups);
           },
           (error) => console.log(error)
         );
