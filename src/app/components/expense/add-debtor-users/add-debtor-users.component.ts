085bf86716c0a5b3b5cea4f1d80ca55d1bbb7165
Cath Gaitan <catu.gaitan@gmail.com>
2023-12-20 20:07:40 -0300

    avanzo strategia

--- src/app/components/expense/add-debtor-users/add-debtor-users.component.ts
+++ src/app/components/expense/add-debtor-users/add-debtor-users.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { ActivatedRoute, RouterModule } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatSelectModule } from '@angular/material/select';
@@ -11,6 +11,11 @@ import { MatInputModule } from '@angular/material/input';
 import { ExpenseService } from '../../../services/expense.service';
 import { Group } from '../../../models/group';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Expense } from '../../../models/expense';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { User } from '../../../models/user';
+import { MatTableModule } from '@angular/material/table';
+
 
 @Component({
   selector: 'app-add-debtor-users',
@@ -24,56 +29,82 @@ import { MatSnackBar } from '@angular/material/snack-bar';
     MatButtonModule,
     MatInputModule,
     RouterModule,
+    MatGridListModule,
+    MatTableModule
   ],
   templateUrl: './add-debtor-users.component.html',
   styleUrl: './add-debtor-users.component.css',
 })
 export class AddDebtorUsersComponent implements OnInit {
-  form: FormGroup;
+  forms: FormArray;
   expenseId: number = 0;
+  expense: Expense = {};
   group: Group = { id: 0, name: '', category: {}, expenses: [], img: '' };
+  dataSource: User[] = [];
+  displayedColumns = ['position', 'username', 'amount', 'isPayed'];
 
   constructor(
     private fb: FormBuilder,
     private route: ActivatedRoute,
     private _expenseService: ExpenseService,
     private _snackBar: MatSnackBar
-  ) {
-    this.form = this.fb.group({
-      amountPayed: ['', Validators.required],
-      isPayed: ['', Validators.required],
-      user: ['', Validators.required],
-    });
-  }
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
       this.expenseId = params['id'];
     });
+    this.getExpense();
     this.getGroup();
+    for(const user of this.group.users || []){
+      this.forms = this.fb.array({
+        username: [user.username],
+        amountPayed: [],
+        isPayed: []
+      })
+      this.forms.push(form);
+    }
+  }
+
+  getExpense(){
+    this._expenseService.getById(this.expenseId).subscribe(
+      (res) => {
+        this.expense = res;
+      },
+      (error) => console.error(error)
+    )
   }
 
   getGroup() {
     this._expenseService.getGroup(this.expenseId).subscribe(
       (res) => {
         this.group = res;
+        this.dataSource = res.users || [];
       },
       (error) => console.error(error)
     );
   }
 
-  addDebtorUser() {
-    const user = this.form.get('user');
-    if (user)
-      this.form.controls['user'].setValue({ id: parseInt(user.value, 10) });
-    this._expenseService
-      .addDebtorUser(this.expenseId, this.form.value)
-      .subscribe(
-        (res) => {
-          this.openSnackBar('Deudor agregado con exito');
-        },
-        (error) => console.error(error)
-      );
+  addDebtorsUsersFixed() {
+    // const user = this.form.get('user');
+    // if (user)
+    //   this.form.controls['user'].setValue({ id: parseInt(user.value, 10) });
+    // this._expenseService
+    //   .addDebtorUser(this.expenseId, this.form.value)
+    //   .subscribe(
+    //     (res) => {
+    //       this.openSnackBar('Deudor agregado con exito');
+    //     },
+    //     (error) => console.error(error)
+    //   );
+
+    for(const form of this.forms){
+      console.log(form);
+    }
+  }
+
+  addDebtorsUsersPercent(){
+
   }
 
   openSnackBar(mensaje: string) {
