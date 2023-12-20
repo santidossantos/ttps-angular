1ad4e8e2295133ff0071f08a3b6884ca817bf423
Sun <santiago.lizondo@gmail.com>
2023-12-20 14:40:48 -0300

    arreglo iconos

--- src/app/components/group/icons-group.ts
+++ src/app/components/group/icons-group.ts
@@ -1,21 +1,17 @@
 class IconsGroup {
   static icons = [
-    // { value: 'wc', name: 'Familia' },
-    // { value: 'local_bar', name: 'Salida' },
-    // { value: 'local_airport', name: 'Viaje' },
-    // { value: 'group', name: 'Otro' },
-
+    { value: 'wc', name: 'Familia' },
+    { value: 'local_bar', name: 'Salida' },
+    { value: 'local_airport', name: 'Viaje' },
     { value: 'people', name: 'Gente' },
     { value: 'school', name: 'Academico' },
-    { value: 'supervisor_account', name: 'Personas' },
+    { value: 'supervisor_account', name: 'Amistades' },
     { value: 'business', name: 'Trabajo' },
-    { value: 'people_outline', name: 'Gente-blanco' },
     { value: 'star', name: 'Estrella' },
-    { value: 'star_border', name: 'Estrella-blanco' },
-    { value: 'business_center', name: 'Maletin' },
-    { value: 'child_friendly', name: 'Cochecito' },
+    { value: 'business_center', name: 'Empleo' },
     { value: 'contacts', name: 'Contactos' },
     { value: 'nature_people', name: 'Naturaleza' },
+    { value: 'group', name: 'Otro' },
   ];
 }
 
