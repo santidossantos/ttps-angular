9ac12e4b27f84d141a428fcf25b22c33fd570ada
Cath Gaitan <catu.gaitan@gmail.com>
2023-12-21 22:02:32 -0300

    anda el equalsAmount

--- src/app/components/expense/add-debtor-users/add-debtor-users.component.ts
+++ src/app/components/expense/add-debtor-users/add-debtor-users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectorRef} from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
@@ -44,7 +44,8 @@ export class AddDebtorUsersComponent implements OnInit {
   group: Group = { id: 0, name: '', category: {}, expenses: [], img: '' };
   dataSource!: MatTableDataSource<any>;
   displayedColumns = ['position', 'user', 'amount', 'isPayed', 'actions'];
-
+  numberOfRows: number = 0;
+  equalAmount: number = 0;
 
   constructor(
     private fb: FormBuilder,
@@ -79,13 +80,31 @@ export class AddDebtorUsersComponent implements OnInit {
     this.debtorsUsers.push(debtorForm);
     this.dataSource = new MatTableDataSource(this.debtorsUsers.controls);
     this.cd.detectChanges();
+    this.numberOfRows++;
+    this.equalAmount = this.expense.amount! / this.numberOfRows;
   }
 
   deleteRow(rowIndex: number): void{
     this.debtorsUsers.removeAt(rowIndex);
     this.dataSource = new MatTableDataSource(this.debtorsUsers.controls);
+    this.numberOfRows--;
+    this.equalAmount = this.expense.amount! / this.numberOfRows;
   }
 
+  addDebtorsUsersEqualsAmounts(){
+    console.log(this.form.value.debtorsUsers)
+    this._expenseService.addDebtorsUsers(this.expenseId, this.form.value.debtorsUsers).subscribe(
+      (res) => {
+        this.openSnackBar('Deudores agregados con exito');
+      },
+      (error) =>{
+        console.error(error)
+        if(error.status == 406){
+          this.openSnackBar('El total de los montos ingresados son menores o mayores al total del gasto');
+        }
+      }
+    );
+  }
   
   addDebtorsUsersFixed() {
     this._expenseService.addDebtorsUsers(this.expenseId, this.form.value.debtorsUsers).subscribe(
@@ -95,34 +114,36 @@ export class AddDebtorUsersComponent implements OnInit {
       (error) =>{
         console.error(error)
         if(error.status == 406){
-          this.openSnackBar('El total de los montos ingresados son menores al total del gasto');
+          this.openSnackBar('El total de los montos ingresados son menores o mayores al total del gasto');
         }
       }
     );
   }
 
   addDebtorsUsersPercent(){
-    const debtors = this.form.value.debtorsUsers;
-    this.debtorsUsers.clear();
+    //console.log("Principio: " ,this.debtorsUsers.value);
+    let debtors = this.debtorsUsers.value; //guardo los valores del form
+    this.debtorsUsers.clear(); //borro lo que esta en el form
     for(const debtor of debtors){
-      let amount = ((this.expense.amount! * debtor.amountPayed)/100);
-      console.log(amount);
+      let amount = 0;
+      //console.log("debtor.amountPayed", debtor.amountPayed)
+      amount = ((this.expense.amount! * debtor.amountPayed)/100);
+      //console.log("Amount", amount);
       const debtorForm = this.fb.group({
         user: debtor.user,
         isPayed: debtor.isPayed,
-        amountPayed: amount
+        amountPayed: amount,
       })
       this.debtorsUsers.push(debtorForm);
     }
-    console.log(this.form.value.debtorsUsers);
-    this._expenseService.addDebtorsUsers(this.expenseId, this.form.value.debtorsUsers).subscribe(
+    this._expenseService.addDebtorsUsers(this.expenseId, this.debtorsUsers.value).subscribe(
       (res) => {
         this.openSnackBar('Deudores agregados con exito');
       },
       (error) =>{
         console.error(error)
         if(error.status == 406){
-          this.openSnackBar('El total de los montos ingresados son menores al total del gasto');
+          this.openSnackBar('El total de los porcentajes ingresados son menores o mayores al 100%');
         }
       }
     );
